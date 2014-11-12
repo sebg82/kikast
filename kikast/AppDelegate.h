daf77066fd0c74753bcb5e9d93d6f745b993ce24
Rocio <sebastien@MacBook-Pro-de-Sebastien.local>
2014-11-12 08:41:02 -0600

    Initial Commit

---
+++ kikast/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  kikast
+//
+//  Created by Rocio on 12/11/14.
+//  Copyright (c) 2014 Seb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ kikast/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  kikast
+//
+//  Created by Rocio on 12/11/14.
+//  Copyright (c) 2014 Seb. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
